2958a7bb3987de004f28f5173e1e8f904f96086b
IlyaLevitskiy <levitskiyilya42@gmail.com>
2022-07-30 19:57:26 +0300

    add link properties and upd ignore

--- src/components/repos/RepoItem.jsx
+++ src/components/repos/RepoItem.jsx
@@ -17,7 +17,7 @@ function RepoItem({ repo }) {
     <div className="mb-2 rounded-md card bg-gray-800 hover:bg-gray-900">
       <div className="card-body">
         <h3 className="mb-2 text-xl font-semibold">
-          <a href={html_url}>
+          <a href={html_url} target="_blank" rel="noreferrer">
             <FaLink className="inline mr-1" />
             {name}
           </a>
